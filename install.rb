d48f01299a582d9e3377ada6ec5580a9e1bbb352
Bryan Mahoney <bryan@goDynamo.com>
2009-04-07 08:36:46 -0400

    removed install code. replaced with install rake task

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
-puts "Puts initializing file copy"
-puts 'copying files'
-PLUGIN_ROOT = File.dirname(__FILE__) + '/../'
-FileUtils.cp_r "#{PLUGIN_ROOT}public/", "#{RAILS_ROOT}", :verbose => true
-FileUtils.cp_r "#{PLUGIN_ROOT}views/layouts/backoffice.html.erb", "#{RAILS_ROOT}/app/views/layouts", :verbose => true
-puts "Files copied. See the ReadMe File."
+# puts "Puts initializing file copy"
+# puts 'copying files'
+# PLUGIN_ROOT = File.dirname(__FILE__) + '/../'
+# FileUtils.cp_r "#{PLUGIN_ROOT}public/", "#{RAILS_ROOT}", :verbose => true
+# FileUtils.cp_r "#{PLUGIN_ROOT}views/layouts/backoffice.html.erb", "#{RAILS_ROOT}/app/views/layouts", :verbose => true
+# puts "Files copied. See the ReadMe File."
