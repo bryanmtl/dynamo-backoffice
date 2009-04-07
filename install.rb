b3f7e17d39f1914d2b9d9170232bab3ede249550
Bryan Mahoney <bryan@goDynamo.com>
2009-04-07 07:52:25 -0400

    fixed install.rb

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 # Install hook code here
 puts "Copying files to public directory:"
-PLUGIN_ROOT = File.dirname(__FILE__) + '/../'
+PLUGIN_ROOT = File.dirname(__FILE__)# + '/../'
 FileUtils.cp "#{PLUGIN_ROOT}public/*", "#{RAILS_ROOT}/public/", :verbose => true
 FileUtils.cp "#{PLUGIN_ROOT}view/layouts/*", "#{RAILS_ROOT}/app/views/layouts/", :verbose => true
 puts "Plugin installed."
